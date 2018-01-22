73e813240c270c8219ef9c04554562fa4f2247d1
Azumi Callanan <azucm@hotmail.com>
2018-01-22 06:16:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -117,7 +117,7 @@ function endGame() {
   ROCKS.forEach(function(rock){
     rock.remove()
   });
-  document.removeEventListener('keydown',moveDodger)
+  document.removeEventListener('keydown', moveDodger)
   return alert('YOU LOSE!')
 }
 
